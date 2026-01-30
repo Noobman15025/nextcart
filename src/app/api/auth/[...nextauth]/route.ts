8cc32c7f89dd1bddac53178f3a3fb8d7480b6b25
Noobman15025 <shahariarshawon.dev@gmail.com>
2026-01-30 23:03:59 +0600

    fixed some error about prisma validation and downgraded from prisma 7 to prisma 6 vversion for better scalablity

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcrypt";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Missing credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        const isValid = await bcrypt.compare(
+          credentials.password,
+          user.password,
+        );
+
+        if (!isValid) {
+          throw new Error("Invalid password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
